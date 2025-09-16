07c8a20aec1b4fd9fba01f95d0125142a3d89554
gustavo.vargens <gustavo.vargens@prodatamobility.com.br>
2025-09-16 11:56:15 -0300

    modified: Adicionando serviços

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -1,10 +1,26 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers()
+    // Com essa alteração, os nomes das propriedades no JSON retornado pela API corresponderão exatamente aos nomes das propriedades na classe C#, respeitando maiúsculas e minúsculas.
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
 
 var app = builder.Build();
 
