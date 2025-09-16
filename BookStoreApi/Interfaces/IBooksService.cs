1b21465062eec1d64a86954d135c72c72a72850c
gustavo.vargens <gustavo.vargens@prodatamobility.com.br>
2025-09-16 11:55:20 -0300

    add: Interface

---
+++ BookStoreApi/Interfaces/IBooksService.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreApi.Models;
+
+namespace BookStoreApi.Interfaces
+{
+    public interface IBooksService
+    {
+        public Task<List<Book>> GetAsync();
+        public Task<Book?> GetAsync(string id);
+        public Task CreateAsync(Book newBook);
+        public Task UpdateAsync(string id, Book updatedBook);
+        public Task DeleteAsync(string id);
+    }
+}
